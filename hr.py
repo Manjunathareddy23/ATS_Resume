43d531b067f012dc2868fef50409abdeb27a9c5c
Kummathi Manjunatha Reddy <141892277+Manjunathareddy23@users.noreply.github.com>
2025-03-06 15:28:52 +0530

    Update hr.py

--- hr.py
+++ hr.py
@@ -16,7 +16,7 @@ if not GEMINI_API_KEY:
 else:
     genai.configure(api_key=GEMINI_API_KEY)
 
-# Function to extract text 
+# Function to extract text from PDF (Resume)
 def extract_text_from_pdf(pdf_file):
     try:
         doc = fitz.open(stream=pdf_file.read(), filetype="pdf")
